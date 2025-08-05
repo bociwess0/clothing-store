42e455443e0bf6d50f9a11214da97c868817a43b
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-08-05 21:51:03 +0200

    adding header and footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>
+    <div className='h-[100vh]'>
       Clothing store
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({subsets: ['latin']});
 
 
 export const metadata: Metadata = {
-  title: "Clothing Store",
-  description: "Modern next.js ecommerce platform",
+  title: APP_NAME,
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
