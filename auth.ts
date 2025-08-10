40da485ab536d6e71b47d0efef3535148da2dd5b
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-08-10 13:40:30 +0200

    fixing warnings

--- auth.ts
+++ auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import { email, jwt } from "zod";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials"
