6f0c4f6fafdd58c7a80ccdc8d9f4fc1ee11a24dc
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-10-19 19:26:11 +0200

    cleaning middleware issues for deploying

--- components/shared/product/AddToCart.tsx
+++ components/shared/product/AddToCart.tsx
@@ -2,8 +2,8 @@
 import { Button } from "@/components/ui/button";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { Cart, CartItem } from "@/types/types";
-import { Minus, Plus } from "lucide-react";
-import React from "react";
+import { Loader, Minus, Plus } from "lucide-react";
+import React, { useTransition } from "react";
 import { toast } from "sonner";
 
 interface Props {
@@ -14,36 +14,41 @@ interface Props {
 export default function AddToCart({ cart, item }: Props) {
   // const router = useRouter();
 
-  const handleAddToCart = async () => {
-    try {
-      const res = await addItemToCart(item);
+  const [isPending, startTransition] = useTransition();
 
-      if (res.success) {
-        toast(`${item.name} is added to cart!`, {
-          description: res.message,
-        });
-      } else {
-        toast(`${item.name} is not added to cart!`, {
-          description: res.message,
+  const handleAddToCart = async () => {
+    startTransition(async () => {
+      try {
+        const res = await addItemToCart(item);
+
+        if (res.success) {
+          toast(`${item.name} is added to cart!`, {
+            description: res.message,
+          });
+        } else {
+          toast(`${item.name} is not added to cart!`, {
+            description: res.message,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        toast("Error occured while trying to add product in cart!", {
+          description: "Bad request!",
         });
       }
-    } catch (error) {
-      console.log(error);
-      toast("Error occured while trying to add product in cart!", {
-        description: "Bad request!",
-      });
-    }
+    });
   };
 
   // Check if item is in cart
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
-    toast(`${item.name} is removed from cart!`, {
-      description: res.message,
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+      toast(`${item.name} is removed from cart!`, {
+        description: res.message,
+      });
+      return;
     });
-
-    return;
   };
 
   const existItem =
@@ -51,12 +56,30 @@ export default function AddToCart({ cart, item }: Props) {
 
   return existItem ? (
     <div>
-      <Button className="cursor-pointer" type="button" variant={"outline"} onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+      <Button
+        className="cursor-pointer"
+        type="button"
+        variant={"outline"}
+        onClick={handleRemoveFromCart}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
-      <Button className="cursor-pointer" type="button" variant={"outline"} onClick={handleAddToCart}>
-        <Plus className="h-4 w-4 " />
+      <Button
+        className="cursor-pointer"
+        type="button"
+        variant={"outline"}
+        onClick={handleAddToCart}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
@@ -65,7 +88,11 @@ export default function AddToCart({ cart, item }: Props) {
       type="button"
       onClick={handleAddToCart}
     >
-      <Plus className="w-4 h-4" />
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="h-4 w-4" />
+      )}{" "}
       Add To Cart
     </Button>
   );
